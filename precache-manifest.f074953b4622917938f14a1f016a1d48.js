7e39f0d11428e9d46b38fea35c7bb0b416898859
Taras <seman6745@gmail.com>
2021-03-29 18:07:20 +0300

    Updates

---
+++ precache-manifest.f074953b4622917938f14a1f016a1d48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec9a6ef98bb29b86a1cc8c700ff9ac2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97b041feec0094e5b531",
+    "url": "./static/css/2.d1ab60d4.chunk.css"
+  },
+  {
+    "revision": "436cb93e5bce332c575d",
+    "url": "./static/css/main.2998c9b7.chunk.css"
+  },
+  {
+    "revision": "97b041feec0094e5b531",
+    "url": "./static/js/2.b12938d3.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.b12938d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436cb93e5bce332c575d",
+    "url": "./static/js/main.545d23b4.chunk.js"
+  },
+  {
+    "revision": "c7ae2510e9f54903a2ed",
+    "url": "./static/js/runtime-main.b51b247a.js"
+  }
+]);
